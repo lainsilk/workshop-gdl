0a8eb434538654e0a14bc144169c48c3e70bd596
Ignacio Pedroza <lain_life@hotmail.com>
2013-06-22 10:55:40 -0500

    last commit i guess

--- workshop/credit-bureau/src/c/cb.c
+++ workshop/credit-bureau/src/c/cb.c
@@ -1,4 +1,5 @@
 /**
+ * gcc cb.c -lws2_32 -o cb
  * Este aplicacion representa a una entidad crediticia la cual
  * concentra todos los creditos otorgados a los clientes de
  * diferentes entidades.
@@ -11,40 +12,207 @@
 #include <winsock2.h>
 #include <unistd.h>
 
-#define PORT 3551 /* El puerto que será abierto */
-#define BACKLOG 2 /* El número de conexiones permitidas */
+#define PORT 3551 /* El puerto que sera abierto */
+#define BACKLOG 2 /* El numero de conexiones permitidas */
+#define bufferSize 100 //Number of bytes to be sent
 
+//This function dictates what kind of processing will be done when the RFC is sent from the client.
+//Up to this point, only lines where the rfc is found ill be sent.
 void doprocessing (int sock)
 {
-    int n;
-    char buffer[256];
+//============================================Global variables definition
 
-    memset(&(buffer), '0', 256);
-    int recvMsgSize;
+    char buffer[bufferSize]; //This is the array which holds the recieved message from the client
+	char file[] = "Loans.txt"; //This is the string which holds the directory location of the file that holds the loans
+	FILE *filePtr; //File pointer used to create the file
+    int recvMsgSize; //Integer that holds the size of the recieved message
 
-    /* Receive message from client */
-    if ((recvMsgSize = recv(sock, buffer, 256, 0)) < 0)
-        perror("ERROR reading to socket 1");
-		int i;
+/*============================================= Receive message from client */
+    if ((recvMsgSize = recv(sock, buffer, bufferSize, 0)) < 0){
+        perror("ERROR reading to socket");
+	}
 
+//===================================================START FILE READING
+	if(buffer[0] == 'r'){
+		if((filePtr = fopen(file, "r")) != NULL)//If the file can open, do the following:
+		{
+		//=======================Variables:
 
-    /* Send received string and receive again until end of transmission */
-    while (recvMsgSize > 0)      /* zero indicates end of transmission */
+			char line[bufferSize]; //Create a char array which will hold each line of the loans file
+			char *readLine = &(line[0]); //Pointer that points to the array of char, used to write each line that was read
+
+			//==================Start Reading lines
+
+			fgets(readLine,bufferSize,filePtr); //This reads the first line, and discards it,  this it to eliminate the 'header' of the
+			//loans file
+
+			//===============================Finding the RFC algorithm
+			while(readLine = fgets(readLine,bufferSize,filePtr)){ //While we still can read lines, we do the following:
+				int indexOfLine; //This int will be used as the index for each line read from the file
+
+				for(indexOfLine=0;line[indexOfLine] != '|';indexOfLine++){} //This for assures that the first column of the file loans is
+				//ignored, in order to find the RFC column
+				int indexOfBuffer=2,numberOfCoincidences=0;//indexOfBuffer is the index that allows us to read all the characters
+				//in the recieved message buffer, numberOfCOincidences counts how many of those characters where equal (Line and buffer)
+
+				for(indexOfLine++;line[indexOfLine] != '|';indexOfLine++){ /*Cycle that reads each of the characters in the buffer and in
+				the column RFC of the line, we compare each character in the next 'if' and when they are the same, we add 1 to the number
+				of coincidences found
+				*/
+					if(line[indexOfLine] == buffer[indexOfBuffer]){
+						numberOfCoincidences++;
+					}
+					indexOfBuffer++; //we move the index of the buffer one space
+				}
+				/*After checking all the line from the first '|' to the second '|', whe check if the number of coincidences is
+				equal to the number of characters in the buffer
+				*/
+				if(numberOfCoincidences == (indexOfBuffer-2)){
+					for(indexOfLine; line[indexOfLine]!='\n';indexOfLine++){}
+					line[indexOfLine++] = ',';
+				//	line[indexOfLine++] = '\0';
+					printf("%s \n", line); //Print the line with the RFC, used for debugging
+					 //With this line we send the line that has the RFC to the socket
+					if(send(sock, readLine, indexOfLine,0) != indexOfLine){
+						perror("Error while writing to the Socket");
+					}
+
+				}
+				for(indexOfLine=0;indexOfLine < bufferSize;indexOfLine++){line[indexOfLine] = '0';} //Erase all infomation in the string
+			}
+				fclose(filePtr);
+		}
+		else //If the file could not be opened, print the message: "Could not open"  and the name of the file
+		{
+			printf("Could not open %s\n", file);
+		}
+	}
+
+	else if (buffer[0] == 'w'){
+	int indexOfBuffer = 2;
+		if((filePtr = fopen(file, "a")) != NULL){//If the file can open, do the following:
+			for(indexOfBuffer = 2; buffer[indexOfBuffer] != '\n' ; indexOfBuffer++){
+				fputc(buffer[indexOfBuffer],filePtr);
+			}
+			printf("Done writing \n");
+			fclose(filePtr);
+		}
+		else{
+			printf("Could not open file");
+		}
+	}
+	else if(buffer[0] == 'c'){
+		//Algorithm to close a credit
+		if((filePtr = fopen(file, "r+")) != NULL)//If the file can open, do the following:
+		{
+		//=======================Variables:
+
+			char line[bufferSize]; //Create a char array which will hold each line of the loans file
+			char *readLine = &(line[0]); //Pointer that points to the array of char, used to write each line that was read
+
+			//==================Start Reading lines
+
+			fgets(readLine,bufferSize,filePtr); //This reads the first line, and discards it,  this it to eliminate the 'header' of the
+			//loans file
+
+			//===============================Finding the RFC algorithm
+			while(readLine = fgets(readLine,bufferSize,filePtr)){ //While we still can read lines, we do the following:
+				int indexOfLine; //This int will be used as the index for each line read from the file
+
+				for(indexOfLine=0;line[indexOfLine] != '|';indexOfLine++){} //This for assures that the first column of the file loans is
+				//ignored, in order to find the RFC column
+				int indexOfBuffer=2,numberOfCoincidences=0;//indexOfBuffer is the index that allows us to read all the characters
+				//in the recieved message buffer, numberOfCOincidences counts how many of those characters where equal (Line and buffer)
+
+				for(indexOfLine++;line[indexOfLine] != '|';indexOfLine++){ /*Cycle that reads each of the characters in the buffer and in
+				the column RFC of the line, we compare each character in the next 'if' and when they are the same, we add 1 to the number
+				of coincidences found
+				*/
+					if(line[indexOfLine] == buffer[indexOfBuffer]){
+						numberOfCoincidences++;
+					}
+					indexOfBuffer++; //we move the index of the buffer one space
+				}
+				/*After checking all the line from the first '|' to the second '|', whe check if the number of coincidences is
+				equal to the number of characters in the buffer
+				*/
+				if(numberOfCoincidences == (indexOfBuffer-2)){
+
+				//==================================================Look for the date
+					int numberOfPipes =0;
+					while(numberOfPipes<3){
+						indexOfLine++;
+						if(line[indexOfLine]=='|'){
+							numberOfPipes++;
+						//	printf("%d\n",numberOfPipes);
+						}
+					}
+					indexOfLine++;
+					indexOfBuffer++;
+					//====new variables
+					numberOfCoincidences = 0;
+					int spaces =0;
+
+					//printf("Buffer's date: %c \nLine's date: %c\n", buffer[indexOfBuffer],line[indexOfLine]);
+
+					for(indexOfLine;line[indexOfLine] != '|';indexOfLine++){
+						printf("%c",line[indexOfLine]);
+						if(line[indexOfLine] == buffer[indexOfBuffer]){
+							numberOfCoincidences++;
+						}
+						indexOfBuffer++;
+						spaces++;
+					}
+					printf("\nNumber of index: %d\n",indexOfLine);
+					if(numberOfCoincidences == spaces){
+						printf("found the date: \n%s\n",line);
+						indexOfLine++;
+						for(indexOfLine; line[indexOfLine]!='|';indexOfLine++){}
+						indexOfLine++;
+						printf("index of line: %d\nchar: %c\n",indexOfLine,line[indexOfLine]);
+/*
+						if(line[indexOfLine] == 'N'){printf("You cannot close an already closed credit\n");}
+						else if(line[indexOfLine]=='Y'){line[indexOfLine] = 'N';}
+						printf("%s\n",line);
+*/
+						//int pos = ftell(filePtr);
+						//printf("%d\n",fseek(filePtr,pos-1,pos));
+						if(fseek(filePtr,-2,SEEK_CUR) == 0){
+							fputc('N',filePtr);
+						}
+						fseek(filePtr,2,SEEK_CUR);
+					}
+				}
+				for(indexOfLine=0;indexOfLine < bufferSize;indexOfLine++){line[indexOfLine] = '0';} //Erase all infomation in the string
+			}
+				fclose(filePtr);
+		}
+		else //If the file could not be opened, print the message: "Could not open"  and the name of the file
+		{
+			printf("Could not open %s\n", file);
+		}
+
+	}
+/*	else if(buffer[0] == 'g'){
+		//Algorithm to get if a client is good or bad
+	}
+*/
+	else{
+		printf("No");
+	}
+
+//=====================================================ENDS FILE READING
+//=====================================================STARTS FILE WRITING
+if((filePtr = fopen(file, "r")) != NULL)//If the file can open, do the following:
     {
-        buffer[recvMsgSize-1]='\0';
 
-        leerfile(buffer,sock);
-        memset(&(buffer),'0',256);
-        /* Echo message back to client */
-        if (send(sock, buffer, recvMsgSize, 0) != recvMsgSize)
-            perror("ERROR writing to socket 2");
-
-        /* See if there is more data to receive */
-        if ((recvMsgSize = recv(sock, buffer, 256, 0)) < 0)
-            perror("ERROR reading to socket 3");
     }
-
-    closesocket(sock);    /* Close client socket */
+    else //If the file could not be opened, print the message: "Could not open"  and the name of the file
+    {
+        printf("Could not open %s\n", file);
+    }
+//====================================================ENDS FILE WRITING
+    closesocket(sock);    /* Close socket */
 }
 
 BOOL initW32()
@@ -74,61 +242,18 @@ BOOL initW32()
 		}
 }
 
-
-
-int leerfile(char* buffer,int sock)
-{
-
-   FILE * pFile;
-   char mystring [100];
-   boolean found=FALSE;
-
-
-   pFile = fopen ("Loans.txt" , "r");
-
-   if (pFile == NULL)
-        perror ("Error opening file");
-      else {
-
-             while (fgets (mystring , 100 , pFile)!= NULL)
-             {
-
-                 if (strncmp(mystring+4,buffer,10)==0){
-                     puts (mystring);
-                     send(sock,mystring,strlen(mystring),0);
-                     found=TRUE;
-                     }
-
-             }
-
-             if (found == FALSE){
-                puts("no se encontro \n");
-                send(sock,"No se encontro\n",15,0);
-                }
-
-           }
-    fclose (pFile);
-   return 0;
-
-}
-//modificar pendejadas
-int modify(char* buffer, int sock)
-{
-
-
-
-}
-
-
-
-
 int main()
 {
 
 	 initW32(); /* Necesaria para compilar en Windows */
-	 int fd, fd2; /* los descriptores de archivos */
-	 struct sockaddr_in server;   /* para la información de la dirección del servidor */
-	 struct sockaddr_in client;/* para la información de la dirección del cliente */
+
+   int fd, fd2; /* los descriptores de archivos */
+
+   /* para la información de la dirección del servidor */
+   struct sockaddr_in server;
+
+   /* para la información de la dirección del cliente */
+   struct sockaddr_in client;
 
    unsigned int sin_size;
 
@@ -144,13 +269,12 @@ int main()
 
    server.sin_port = htons(PORT);
 
-   server.sin_addr.s_addr = INADDR_ANY;/* INADDR_ANY coloca nuestra dirección IP automáticamente */
-
+   server.sin_addr.s_addr = INADDR_ANY;
+   /* INADDR_ANY coloca nuestra dirección IP automáticamente */
 
    //bzero(&(server.sin_zero),8);
-
-   memset(&(server.sin_zero), '0', 8); /* escribimos ceros en el reto de la estructura */
-
+   memset(&(server.sin_zero), '0', 8);
+   /* escribimos ceros en el reto de la estructura */
 
 
    /* A continuación la llamada a bind() */
@@ -172,11 +296,10 @@ int main()
          exit(-1);
       }
 
-      printf("Se obtuvo una conexion desde %s\n\n", inet_ntoa(client.sin_addr) );
+      printf("Se obtuvo una conexion desde %s\n", inet_ntoa(client.sin_addr) );
       /* que mostrará la IP del cliente */
 
-      //send(fd2,"Bienvenido a mi servidor Lain.\n",99,0);
-
+      //send(fd2,"Bienvenido a mi servidor.\n",bufferSize,0);
       /* que enviará el mensaje de bienvenida al cliente */
 
       doprocessing(fd2);
